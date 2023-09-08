26bdb5aed174dd8de9c555b9f92f9fccccd49425
Xavier Demompion <xwar_srkn@hotmail.com>
2023-09-08 19:12:20 +0200

    init lib

---
+++ lib/phoenix_ddos.ex
@@ -0,0 +1,18 @@
+defmodule PhoenixDdos do
+  @moduledoc """
+  Documentation for `PhoenixDdos`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PhoenixDdos.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/phoenix_ddos_test.exs
@@ -0,0 +1,8 @@
+defmodule PhoenixDdosTest do
+  use ExUnit.Case
+  doctest PhoenixDdos
+
+  test "greets the world" do
+    assert PhoenixDdos.hello() == :world
+  end
+end
